49e7a24bb485f30a1d0eb7de18d1fe9abbc5b794
Alessandro <s333956@studenti.polito.it>
2025-10-26 14:51:06 +0100

    Semplification on level 2

--- Assets/Scripts/SpawnerOggetti.cs
+++ Assets/Scripts/SpawnerOggetti.cs
@@ -16,17 +16,18 @@ public class SpawnerOggetti:MonoBehaviour{          // Gestisce un qualsiasi ogg
         routine=true;
         StartCoroutine(InserisciNemici());}          // Avvio
     private void OnBecameInvisible(){
-        routine=false;}           // Stop
+        routine=false;}                                    // Stop
 
 //////////////////////////////////////////////// ATTESA ////////////////////////////////////////////////////////////////
     private IEnumerator InserisciNemici(){
         var index=0;
         
         while(routine){
-            if(index==nemiciDaAggiungere.Length){    // All'infinito, finché non viene fermata
+            if(index==nemiciDaAggiungere.Length){                      // All'infinito, finché non viene fermata
                 index=0;}
             
-            nemiciDaAggiungere[index].position=posLoad;
-            nemiciDaAggiungere[index].gameObject.SetActive(true);          // Via
-            yield return new WaitForSeconds(2);
+            if(!nemiciDaAggiungere[index].gameObject.activeSelf){            // Se non è già fuori
+                nemiciDaAggiungere[index].position=posLoad;
+                nemiciDaAggiungere[index].gameObject.SetActive(true);          // Via
+                yield return new WaitForSeconds(2);}
             index++;}}}
